339447937cd528f3b590e6f86b8866a73abc0239
BugHunter-X <askzenai@gmail.com>
2024-12-16 06:32:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,1 @@
+While using Expo CLI, I encountered an unusual error during the build process.  The error message was vague and didn't point to a specific problem in my code.  The build process would hang indefinitely, without any further error messages or output.
